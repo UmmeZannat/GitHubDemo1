84d6ded17389998b8dbdd69008f3ae1946056ccf
UmmeZannat <76455582+UmmeZannat@users.noreply.github.com>
2020-12-23 14:02:10 -0500

    First commit

---
+++ GitHubDemo1/src/main/java/com/pages/LoginPage1.java
@@ -0,0 +1,8 @@
+package com.pages;
+
+public class LoginPage1 {
+	public void loginPage1() {
+		System.out.println("This is loginPage1");
+
+}
+}
---
+++ GitHubDemo1/src/test/java/com/testcases/LoginTest1.java
@@ -0,0 +1,8 @@
+package com.testcases;
+
+public class LoginTest1 {
+	public void loginTest() {
+		System.out.println("This is loginTest");
+
+}
+}
